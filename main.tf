41b225348510a919600cd452523e2d8764d03e39
nagesh <nduraphe@gmail.com>
2022-12-10 02:06:19 +0530

    final update

--- main.tf
+++ main.tf
@@ -11,3 +11,4 @@ resource "aws_instance" "MyWebServer" {
     Name = "MyWebServer"
   }
 }
+
--- modules/ami/latest_ami.tf
+++ modules/ami/latest_ami.tf
@@ -4,7 +4,7 @@ data "aws_ami" "latest" {
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-kernel-*-hvm-*-arm64-gp2"]
+    values = ["amzn2-ami-kernel-*-hvm-*-x86_64-gp2"]
   }
 }
 
